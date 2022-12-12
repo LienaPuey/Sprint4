09214808b91de5fa048092759da47573ba9d2db7
unknown <lienapuey@gmail.com>
2022-12-12 15:57:31 +0100

    first steps

--- src/films.js
+++ src/films.js
@@ -1,18 +1,26 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result = array.map((movie) =>movie.director);
+  // console.log("EXERCICE 1 ->", result);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+ let result = array.filter((movie)=> movie.director === director);
+
+ return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+ let directorMovies = getMoviesFromDirector(array, director);
+ let average = directorMovies.reduce((acc, movie) => {
+    acc+= movie.score;
+    return acc;
+ }, 0);
+ let score = Number((average / directorMovies.length).toFixed(2));
+ return score;
 }
 
 // Exercise 4:  Alphabetic order by title 
